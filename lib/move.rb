3e3f6de3fdd3b700982a45b1919e9eab6c02345d
lauris <lauris.kruusamae@mobisolutions.com>
2012-10-09 21:03:24 +0300

    improved move class

--- lib/move.rb
+++ lib/move.rb
@@ -3,5 +3,18 @@ require "gapbammon/version"
 module Gapbammon
   class Move
     attr_accessor :player, :position, :steps
+
+    def initialize(player, position, steps)
+      raise "player not an instance of Player" unless player.is_a? Player
+      raise "invalid position" unless (-1..25).include? position
+      raise "invalid number of steps" unless (0..6).include? steps
+      @player = player
+      @position = position
+      @steps = steps
+    end
+
+    def to_s
+      "player: #{@player}, position: #{@position}, steps: #{@steps}" 
+    end
   end
 end
